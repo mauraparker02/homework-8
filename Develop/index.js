a122d9457c79c2a9bf79ec606563a1bd3918845f
Maura Parker <mauraparker02@gmail.com>
2020-04-15 17:41:02 -0400

    setting up an inquierer for user data and more pseduo code for creating the Readme Template

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,113 @@
-const questions = [
+// const questions = [
 
-];
+// ];
 
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//user input inquier 
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "Title",
+            message: "Title of application?"
+        },
+        {
+            type: "input",
+            name: "Description",
+            message: "What does your application do?"
+        },
+        {
+            type: "input",
+            name: "Table of Contents",
+            message: "What are all the sections of your application?"
+        },
+        {
+            type: "input",
+            name: "Installation",
+            message: "Installation information?"
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "How do you use the application"
+        },
+        {
+            type: "input",
+            name: "License",
+            message: "License for application."
+        },
+        {
+            type: "input",
+            name: "Contributing",
+            message: "Who is contributing"
+        },
+        {
+            type: "input",
+            name: "Tests",
+            message: "What are tests you've run on your application"
+        },
+        {
+            type: "input",
+            name: "Questions",
+            message: "?"
+        },
+    ]);
 }
 
-function init() {
+// this function will generate the Readme insert template here and template literal to return a readme template filled in 
 
+function generateREADME(answers) {
+    return `
+    ```
+    "GIVEN the developer has a GitHub profile and a repository"
+    
+    "WHEN prompted for the developer's GitHub username and repo specific information"
+    
+    "THEN a README for the repo is generated"
+    ```
+    - - -`;
 }
 
-init();
+//getting application to run Async
+
+// async function init() {
+//     console.log("hi")
+//     try {
+//         const answers = await promptUser();
+
+//         const html = generateREADME(answers);
+
+//         await writeFileAsync("index.html", html);
+
+//         console.log("Successfully wrote to index.html");
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// init();
+
+
+
+
+
+
+
+
 
 
 //Pseduo Code: 
@@ -60,64 +158,3 @@ init();
 
 // Refer to the [Good README guide](../../01-HTML-Git-CSS/04-Supplemental/Good-README-Guide/README.md).
 
-// ## Business Context
-
-// When creating an open source project on GitHub, it is important to have a quality README with information about the app--what is the app for, how to use the app, how to install it, how to report issues, and how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
-
-// ## Minimum Requirements
-
-// * Functional, deployed application.
-
-// * GitHub repository with a unique name and a README describing project.
-
-// * The generated README includes the following sections: 
-//   * Title
-//   * Description
-//   * Table of Contents
-//   * Installation
-//   * Usage
-//   * License
-//   * Contributing
-//   * Tests
-//   * Questions
-
-// * The generated README includes 1 badge that's specific to the repository.
-
-// ```
-// GIVEN the developer has a GitHub profile and a repository
-
-// WHEN prompted for the developer's GitHub username and repo specific information
-
-// THEN a README for the repo is generated
-// ```
-// - - -
-
-// ## Commit Early and Often
-
-// One of the most important skills to master as a web developer is version control. Building the habit of committing via Git is important for two reasons:
-
-// * Your commit history is a signal to employers that you are actively working on projects and learning new skills.
-
-// * Your commit history allows you to revert your code base in the event that you need to return to a previous state.
-
-// Follow these guidelines for committing:
-
-// * Make single-purpose commits for related changes to ensure a clean, manageable history. If you are fixing two issues, make two commits.
-
-// * Write descriptive, meaningful commit messages so that you and anyone else looking at your repository can easily understand its history.
-
-// * Don't commit half-done work, for the sake of your collaborators (and your future self!).
-
-// * Test your application before you commit to ensure functionality at every step in the development process.
-
-// We would like you to have more than 200 commits by graduation, so commit early and often!
-
-// ## Submission on BCS
-
-// You are required to submit the following:
-
-// * A video demonstrating the entirety of the app's functionality 
-
-// * A generated README.md file for a project repo.
-
-// * The URL of the GitHub repository
